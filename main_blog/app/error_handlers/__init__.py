d15f33f86465de39231adfc8ba7ac8c7d00d8cdd
Даниил <mahovplus66@mail.ru>
2025-04-01 17:09:39 +0300

    Full

--- main_blog/app/error_handlers/__init__.py
+++ main_blog/app/error_handlers/__init__.py
@@ -1,6 +1,6 @@
-from page_not_found import not_found
-from access_denied import no_access
-from something_wrong import server_error
+from app.error_handlers.page_not_found import not_found
+from app.error_handlers.access_denied import no_access
+from app.error_handlers.something_wrong import server_error
 
 exception_handlers = {
     404: not_found,
