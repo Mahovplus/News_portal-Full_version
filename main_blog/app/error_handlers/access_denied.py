b6eed764a637a5d4f014c30981755b03ea044430
Даниил <mahovplus66@mail.ru>
2025-04-04 09:11:05 +0300

    Changed path to templates are errors

--- main_blog/app/error_handlers/access_denied.py
+++ main_blog/app/error_handlers/access_denied.py
@@ -3,5 +3,5 @@ from core.config import TemplateResponse
 
 
 async def no_access(request: Request, exc: HTTPException):
-    return TemplateResponse('error_pages/page_403.jinja2',
+    return TemplateResponse('C:\\Python_Projects\\News_portal\\main_blog\\templates\\error_pages\\page_403.jinja2',
                             {'request': request})
--- main_blog/app/error_handlers/something_wrong.py
+++ main_blog/app/error_handlers/something_wrong.py
@@ -4,5 +4,5 @@ from core.config import TemplateResponse
 
 
 async def server_error(request: Request, exc: HTTPException):
-    return TemplateResponse('error_pages/page_500.jinja2',
+    return TemplateResponse('C:\\Python_Projects\\News_portal\\main_blog\\templates\\error_pages\\page_500.jinja2',
                             {'request': request})
